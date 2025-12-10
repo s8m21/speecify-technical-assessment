61d1aac5450baa1b3617e081b64b74ab8025fd25
Spandan Maaheshwari <127258950+s8m21@users.noreply.github.com>
2025-12-10 15:44:20 -0500

    Implement basic LRU cache functionality
    Implemented the basic structure of the LRU cache with methods to check existence, retrieve, and set items.

--- src/lru.py
+++ src/lru.py
@@ -13,28 +13,21 @@ class LRUCache:
     """
 
     def __init__(self, item_limit: int):
-        # TODO: implement this function
         self.item_limit = item_limit
         self.cache = OrderedDict()
-    
-        #raise NotImplementedError()
-
+        
     def has(self, key: str) -> bool:
-        # TODO: implement this function
         if key in self.cache:
             self.cache.move_to_end(key)
             return True
         return False
-        #raise NotImplementedError()
-
+    
     def get(self, key: str) -> Optional[Any]:
-        # TODO: implement this function
         if key not in self.cache:
             return None
         
         self.cache.move_to_end(key)
         return self.cache[key]
-        #raise NotImplementedError()
 
     def set(self, key: str, value: Any):
         # TODO: implement this function
@@ -42,8 +35,7 @@ class LRUCache:
             self.cache[key] = value
             self.cache.move_to_end(key)
             return
-        #raise NotImplementedError()
-
+      
         if len(self.cache) >= self.item_limit:
             self.cache.popitem(last=False)
         self.cache[key] = value
